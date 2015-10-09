4d180ac4a075ec196aa674926b5e33a8b5be74b7
anako18 <nuka97@yandex.ru>
2015-10-09 19:55:58 +0300

    Quick_sort_5: Добавлены тесты в main_quick_sort.cpp

--- main_quick_sort.cpp
+++ main_quick_sort.cpp
@@ -1,6 +1,17 @@
 #include "quick_sort.h"
+#include <iostream>
+using namespace std;
 
 int main() {
 
+	int a[6]{ 6, 2, 3, 7, 1, 5 };
+	my_qsort(a, 6);
+	print(a, 6);
+	cout << endl;
+
+	int b[6]{ 1, 5, 6, 2, -1, 3 };
+	my_qsort(b, 6);
+	print(b, 6);
+	cout << endl;
 
 }
