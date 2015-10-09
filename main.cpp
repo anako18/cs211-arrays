1c774d1c76400a166428993b0e8bc77e8f407e0e
anako18 <nuka97@yandex.ru>
2015-10-09 16:58:38 +0300

    Arrays: Функции протестированы в main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 #include <cassert>
+#include "arrays.h"
 
 using namespace std;
 
 int main() {
+
+	//tasks 1-4
+	task_1();
+	task_2();
+	assert(!task_3_zero());
+	task_4();
+
+	//task_5_copy
+	int s = 5;
+	int a[5]{ 1, 2, 3, 4, 5 };
+	int * b = new int[s];
+	task_5_copy(a, b, s);
+	for (int i = 0; i < s; i++)
+		assert(a[i] == b[i]);
+
+	//task_6_poor_copy
+	int *c = new int[s];
+	task_6_poor_copy(b, c, s);
+	for (int i = 0; i < s; i++)
+		assert(b[i] == c[i]);
+
+	//task_7_print
+	task_7_print(c, s);
+	cout << endl;
+
+
 
 }
